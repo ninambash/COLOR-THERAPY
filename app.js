5bb8be831f9bece9ebe905aa95b0e7506ebd779a
ninambash <ninambash@gmail.com>
2022-12-04 10:11:37 -0800

    updated css

---
+++ app.js
@@ -0,0 +1,5 @@
+console.log("hello World")
+const gameColors = ["red","blue","green","yellow"]
+const message = document.querySelector(".message")
+const gamearea = document.querySelector(".gamearea")
+const button = document.querySelector("button")
