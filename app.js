77ac7848be1fc6ea0257f974cb4ee6a841db142a
ninambash <ninambash@gmail.com>
2022-12-04 10:19:10 -0800

    updated the setup function

--- app.js
+++ app.js
@@ -4,3 +4,20 @@ const gameColors = ["red","blue","green","yellow"]
 const message = document.querySelector(".message")
 const gamearea = document.querySelector(".gamearea")
 const button = document.querySelector("button")
+
+///add event listeners 
+window.addEventListener("load",setup)
+///create a function for set up
+function setup(){
+  //check if its working
+  console.log("page Loaded")
+  for(let X = 0; X < gameColors.length; X++){
+    let div = eleFactory("div")
+    div.style.backgroundColor = gameColors[X]
+    div.classList.add("box")
+    div.style.opacity = "0.5"
+    div.myColor = gameColors[X]
+    div.addEventListener("click",checkAnswer)
+    gamearea.appendChild(div)
+  }
+}
