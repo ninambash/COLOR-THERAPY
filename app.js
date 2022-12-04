3f94be1bad9b2f5eaaf66ca817a9bce105a5ac36
ninambash <ninambash@gmail.com>
2022-12-04 10:24:34 -0800

    updated app.js

--- app.js
+++ app.js
@@ -4,9 +4,45 @@ const gameColors = ["red","blue","green","yellow"]
 const message = document.querySelector(".message")
 const gamearea = document.querySelector(".gamearea")
 const button = document.querySelector("button")
+let gameClicks = []
+let userClicks = []
+let inPlay = false
+let playNum = 5
 
 ///add event listeners 
 window.addEventListener("load",setup)
+button.addEventListener("click",function(){
+  if(!inPlay){
+    player()
+  }
+ })
+ function player(){
+   button.disabled = true
+       gameClicks = []
+       userClicks = []
+       runSequence(playNum)
+ }
+ function runSequence(num){
+  let squares = document.querySelectorAll(".box")
+  num--
+  if(num < 0){
+    inPlay = true
+    return 
+  }
+  let randomNum = Math.floor(Math.random() * gameColors.length)
+  console.log(squares[randomNum])
+  gameClicks.push(gameColors[randomNum])
+  console.log(gameClicks)
+  squares[randomNum].style.opacity = "1"
+  setTimeout(function(){
+    squares[randomNum].style.opacity = "0.5"
+    setTimeout(function(){
+      runSequence(num)
+
+    },100)
+  },500)
+
+}
 ///create a function for set up
 function setup(){
   //check if its working
@@ -20,4 +56,24 @@ function setup(){
     div.addEventListener("click",checkAnswer)
     gamearea.appendChild(div)
   }
+}
+function checkAnswer(e){
+  if(inPlay){
+    let el = e.target
+    userClicks.push(el.myColor)
+    el.style.opacity = "1"
+    setTimeout(function(){
+      el.style.opacity = "0.5"
+    },500)
+    if(userClicks.length == gameClicks.length){
+      inPlay = false
+      endGame()
+    }
+
+  }
+  console.log(userClicks)
+}
+function eleFactory(elType){
+  let ele = document.createElement(elType)
+  return ele
 }
