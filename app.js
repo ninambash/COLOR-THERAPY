ef2afca4b1d5b02ce472efaef2805964ea261b05
ninambash <ninambash@gmail.com>
2022-12-04 10:13:55 -0800

    added dom content

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 console.log("hello World")
+////connecting DOM elements
 const gameColors = ["red","blue","green","yellow"]
 const message = document.querySelector(".message")
 const gamearea = document.querySelector(".gamearea")
