6611653bc06e3c96a57334c3b819b8335a54bc67
ninambash <ninambash@gmail.com>
2022-12-04 11:26:18 -0800

    updated status of the game

--- app.js
+++ app.js
@@ -1,100 +1,97 @@
-console.log("hello World")
+console.log("hello World");
 ////connecting DOM elements
-const gameColors = ["red","blue","green","yellow"]
-const message = document.querySelector(".message")
-const gamearea = document.querySelector(".gamearea")
-const button = document.querySelector("button")
-let gameClicks = []
-let userClicks = []
-let inPlay = false
-let playNum = 5
+const gameColors = ["red", "blue", "green", "yellow"];
+const message = document.querySelector(".message");
+const gamearea = document.querySelector(".gamearea");
+const button = document.querySelector("button");
+let gameClicks = [];
+let userClicks = [];
+let inPlay = false;
+let playNum = 2;
 
-///add event listeners 
-window.addEventListener("load",setup)
-button.addEventListener("click",function(){
-  if(!inPlay){
-    player()
+///add event listeners
+window.addEventListener("load", setup);
+button.addEventListener("click", function () {
+  if (!inPlay) {
+    player();
   }
- })
- function player(){
-   button.disabled = true
-   button.style.display = 'none'
-   player()
-   gameClicks = []
-   userClicks = []
-   runSequence(playNum)
-       
- }
- function runSequence(num){
-  let squares = document.querySelectorAll(".box")
-  num--
-  if(num < 0){
-    inPlay = true
-    return 
+});
+
+function player() {
+  button.disabled = true;
+  button.style.display = "none"
+  messanger('Match Pattern')
+  gameClicks = [];
+  userClicks = [];
+  runSequence(playNum);
+}
+function runSequence(num) {
+  let squares = document.querySelectorAll(".box");
+  num--;
+  if (num < 0) {
+    inPlay = true;
+    return;
   }
-  let randomNum = Math.floor(Math.random() * gameColors.length)
-  console.log(squares[randomNum])
-  gameClicks.push(gameColors[randomNum])
-  console.log(gameClicks)
-  squares[randomNum].style.opacity = "1"
-  setTimeout(function(){
-    squares[randomNum].style.opacity = "0.5"
-    setTimeout(function(){
-      runSequence(num)
-
-    },100)
-  },500)
-
+  let randomNum = Math.floor(Math.random() * gameColors.length);
+  console.log(squares[randomNum]);
+  gameClicks.push(gameColors[randomNum]);
+  console.log(gameClicks);
+  squares[randomNum].style.opacity = "1";
+  setTimeout(function () {
+    squares[randomNum].style.opacity = "0.5";
+    setTimeout(function () {
+      runSequence(num);
+    }, 100);
+  }, 500);
 }
 ///create a function for set up
-function setup(){
+function setup() {
   //check if its working
-  console.log("page Loaded")
-  for(let X = 0; X < gameColors.length; X++){
-    let div = eleFactory("div")
-    div.style.backgroundColor = gameColors[X]
-    div.classList.add("box")
-    div.style.opacity = "0.5"
-    div.myColor = gameColors[X]
-    div.addEventListener("click",checkAnswer)
-    gamearea.appendChild(div)
+  console.log("page Loaded");
+  for (let X = 0; X < gameColors.length; X++) {
+    let div = eleFactory("div");
+    div.style.backgroundColor = gameColors[X];
+    div.classList.add("box");
+    div.style.opacity = "0.5";
+    div.myColor = gameColors[X];
+    div.addEventListener("click", checkAnswer);
+    gamearea.appendChild(div);
   }
 }
-function checkAnswer(e){
-  if(inPlay){
-    let el = e.target
-    userClicks.push(el.myColor)
-    el.style.opacity = "1"
-    setTimeout(function(){
-      el.style.opacity = "0.5"
-    },500)
-    if(userClicks.length == gameClicks.length){
-      inPlay = false
-      endGame()
+//////////////this is where the user paticipates
+function checkAnswer(e) {
+  if (inPlay) {
+    let el = e.target;
+    userClicks.push(el.myColor);
+    el.style.opacity = "1";
+    setTimeout(function () {
+      el.style.opacity = "0.5";
+    }, 500);
+    if (userClicks.length == gameClicks.length) {
+      inPlay = false;
+      endGame();
     }
-
   }
-  console.log(userClicks)
+  console.log(userClicks);
 }
-////user message function
-function messager(mes){
-  message.innerHTML = mes
+function messanger(mes){
+  message.innerHTML =mes
 }
-
 //////end game function
-function endGame(){
-  console.log("game over")
-  button.disabled = false
-  button.style.display = "blockS"
-  if(userClicks.toString() == gameClicks.toString()){
-    playNum++
-    messager("Match Pattern")
-    console.log("correct")
+function endGame() {
+  console.log("game over");
+  button.disabled = false;
+  button.style.display = "block"
+  //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toString
+  if (userClicks.toString() == gameClicks.toString()) {
+    playNum++; /////increase the play number if they got correct
+    messanger('correct') ////let the player know status of the game
   }else{
-    console.log("not correct")
+    messanger("not correct");
   }
 }
-function eleFactory(elType){
-  let ele = document.createElement(elType)
-  return ele
+
+function eleFactory(elType) {
+  let ele = document.createElement(elType);
+  return ele;
 }
